b31e1e34592066a104d7d538e5db584f0a10b556
Konrad Malawski <konrad.malawski@project13.pl>
2011-01-29 20:18:01 +0100

    readme

--- g73_light_down.sh
+++ g73_light_down.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-current=`cat /home/ktoso/.g73_key_light_state`;
+current=`cat /sys/class/leds/asus::kbd_backlight/brightness`;
 
 if [ "$current" -gt 0 ]
 then
     current=`expr $current - 1`;
 fi;
 
-echo $current > /home/ktoso/.g73_key_light_state
 sudo /home/ktoso/g73/g73_light_.sh "$current"
--- g73_light_up.sh
+++ g73_light_up.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-current=`cat /home/ktoso/.g73_key_light_state`;
+current=`cat /sys/class/leds/asus::kbd_backlight/brightness`;
 
 if [ "$current" -lt 3 ]
 then
     current=`expr $current + 1`;
 fi;
 
-echo $current > /home/ktoso/.g73_key_light_state
 sudo /home/ktoso/g73/g73_light_.sh "$current" 2> /home/ktoso/g73/fail.log
